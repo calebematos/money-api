4b485f8d1cf94a6d96fa019dd969f9bf05f0e2de
Lucas Calebe <calebematos@gmail.com>
2018-10-09 22:09:10 -0300

    ajuste

--- src/main/java/com/calebematos/api/service/LancamentoService.java
+++ src/main/java/com/calebematos/api/service/LancamentoService.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
